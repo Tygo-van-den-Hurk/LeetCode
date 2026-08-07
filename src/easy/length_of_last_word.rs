3726d0d6811c0b40aa56156303d653f7e1f4b2e9
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-08-07 17:19:40 +0200

    feat: complete 'Length of Last Word' problem
    Given a string `s` consisting of words and spaces, return the length of
    the last word in the string. A word is a maximal consisting of non-space
    characters only.

---
+++ src/easy/length_of_last_word.rs
@@ -0,0 +1,83 @@
+//!  # Length of Last Word
+//! 
+//! Given a string `s` consisting of words and spaces, return the length of
+//! the last word in the string. A word is a maximal consisting of non-space
+//! characters only.
+//! 
+//! ## Examples
+//! 
+//! ```
+//! # use leetcode::easy::length_of_last_word::Solution;
+//! 
+//! let input = "Hello World".to_string();
+//! let expected = 5;
+//! let result = Solution::length_of_last_word(input);
+//! assert_eq!(result, expected);
+//! 
+//! let input = "   fly me   to   the moon  ".to_string();
+//! let expected = 4;
+//! let result = Solution::length_of_last_word(input);
+//! assert_eq!(result, expected);
+//! 
+//! let input = "luffy is still joyboy".to_string();
+//! let expected = 6;
+//! let result = Solution::length_of_last_word(input);
+//! assert_eq!(result, expected);
+//! ```
+//! 
+
+pub struct Solution;
+
+impl Solution {
+    pub fn length_of_last_word(input: String) -> i32 {
+        let mut input = input.chars().rev();
+
+        // ignore the whitespace at the end
+        for character in &mut input {
+            if ! character.is_whitespace() {
+                break;
+            }
+        }
+
+        // now that we've read 1 non-whitespace character, we count
+        let mut size = 1;
+        for character in input {
+            if !character.is_whitespace() {
+                size += 1; 
+            } else {
+                break;
+            }
+        }
+        
+        return size;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_case_1() {
+        let input = "Hello World".to_string();
+        let expected = 5;
+        let result = Solution::length_of_last_word(input);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_case_2() {
+        let input = "   fly me   to   the moon  ".to_string();
+        let expected = 4;
+        let result = Solution::length_of_last_word(input);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_case_3() {
+        let input = "luffy is still joyboy".to_string();
+        let expected = 6;
+        let result = Solution::length_of_last_word(input);
+        assert_eq!(result, expected);
+    }
+}
--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -4,6 +4,7 @@
 //!
 
 pub mod find_the_index_of_the_first_occurrence_in_a_string;
+pub mod length_of_last_word;
 pub mod longest_common_prefix;
 pub mod merge_two_sorted_lists;
 pub mod plus_one;
