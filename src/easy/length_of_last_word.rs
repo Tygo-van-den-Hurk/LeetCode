5643a096edd3b5ec144952f1e927af2d0830a177
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-08-07 17:22:37 +0200

    style: just ran `cargo fmt` to make ci happy
    I don't know why I added this. It is leet code, it doesn't really matter.

--- src/easy/length_of_last_word.rs
+++ src/easy/length_of_last_word.rs
@@ -1,30 +1,30 @@
 //!  # Length of Last Word
-//! 
+//!
 //! Given a string `s` consisting of words and spaces, return the length of
 //! the last word in the string. A word is a maximal consisting of non-space
 //! characters only.
-//! 
+//!
 //! ## Examples
-//! 
+//!
 //! ```
 //! # use leetcode::easy::length_of_last_word::Solution;
-//! 
+//!
 //! let input = "Hello World".to_string();
 //! let expected = 5;
 //! let result = Solution::length_of_last_word(input);
 //! assert_eq!(result, expected);
-//! 
+//!
 //! let input = "   fly me   to   the moon  ".to_string();
 //! let expected = 4;
 //! let result = Solution::length_of_last_word(input);
 //! assert_eq!(result, expected);
-//! 
+//!
 //! let input = "luffy is still joyboy".to_string();
 //! let expected = 6;
 //! let result = Solution::length_of_last_word(input);
 //! assert_eq!(result, expected);
 //! ```
-//! 
+//!
 
 pub struct Solution;
 
@@ -34,7 +34,7 @@ impl Solution {
 
         // ignore the whitespace at the end
         for character in &mut input {
-            if ! character.is_whitespace() {
+            if !character.is_whitespace() {
                 break;
             }
         }
@@ -43,12 +43,12 @@ impl Solution {
         let mut size = 1;
         for character in input {
             if !character.is_whitespace() {
-                size += 1; 
+                size += 1;
             } else {
                 break;
             }
         }
-        
+
         return size;
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! This module if for leetcode exercises I solve in Rust. It contains all my
 //! solved exercises just for fun. Checkout [my leetcode profile][leetcode] to
 //! follow my progress.
-//! 
+//!
 //! There are a 3 categories: [easy], [medium], [hard]. Increasing in difficulty
 //! respectively.
 //!
